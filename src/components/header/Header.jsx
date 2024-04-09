7fb707bb17494dcd711b8baaca8cfd9b85904d78
Joshua Zega <lazyreflect@gmail.com>
2024-04-09 23:55:49 +0700

    Adding a small design to the App

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import "./header.css";
 
-function Header() {
+
+const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">JNZega</a>
+
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 }
 
