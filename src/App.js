7a5e3fe499df3262aab00b44f843221e5d94e0df
Joshua Zega <lazyreflect@gmail.com>
2024-04-10 22:04:00 +0700

    Creating home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+
+      <main className='main'>
+        <Home />
+      </main>
     </>
   );
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,33 @@
+
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a 
+        href="https://instagram.com/jnzega/" 
+        className="home__social-icon" 
+        target="_blank"
+        >
+            <i className="uil uil-instagram"></i>
+        </a>
+
+        <a 
+        href="https://linkedin.com/in/jnzega" 
+        className="home__social-icon" 
+        target="_blank"
+        >
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a 
+        href="https://github.com/jnzega" 
+        className="home__social-icon" 
+        target="_blank"
+        >
+            <i class="uil uil-github"></i>
+        </a>
+    </div>
+  );
+}
+
+export default Social;
