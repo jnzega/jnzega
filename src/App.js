6e450e25d9a54cbe7845f17fc40dfe4f3edc02f1
Joshua Zega <hedywidja321@gmail.com>
2024-04-11 22:01:12 +0700

    Creating services section on the main page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/home/Home';
 import './App.css';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Services />
       </main>
     </>
   );
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,170 @@
+import {useState} from "react";
+import "./services.css"
+
+const Services = () => {
+    const [toggleState, setToggleState] = useState(0);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
+    return (
+        <section className="services section" id="services">
+            <h2 className="section__title">Services</h2>
+            <span className="section__subtitle">What i offer</span>
+
+            <div className="services__container container grid">
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-arrow services__icon"></i>
+                        <h3 className="services__title">Web <br /> Developer</h3>
+                    </div>
+
+                    <span className="services__button" onClick={() => toggleTab(1)}>
+                        View More <i className="uil uil-arrow-right services__button-icon"></i>
+                    </span>
+
+                    <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
+                        <div className="services__modal-content">
+                            <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+
+                            <h3 className="services__modal-title">Web Developer</h3>
+                            <p className="services__modal-description">
+                                Service with various experiences. Providing first to clients and partners.
+                            </p>
+
+                            <ul className="services__modal-services grid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Designing user interface for your app.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Developing a page for your app.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Building your database from scratch.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Creating backend services for your app</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Designing your app portfolio.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-web-grid services__icon"></i>
+                        <h3 className="services__title">Product <br/> Designer</h3>
+                    </div>
+
+                    <span onClick={() => toggleTab(2)} className="services__button">
+                        View More <i className="uil uil-arrow-right services__button-icon"></i>
+                    </span>
+
+                    <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
+                        <div className="services__modal-content">
+                            <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+
+                            <h3 className="services__modal-title">Product Designer</h3>
+                            <p className="services__modal-description">
+                                Service with various experiences. Providing first to clients and partners.
+                            </p>
+
+                            <ul className="services__modal-services grid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Designing user interface for your app.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Developing a page for your app.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Building your database from scratch.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Creating backend services for your app</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Designing your app portfolio.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-arrow services__icon"></i>
+                        <h3 className="services__title">UI <br/> Designer</h3>
+                    </div>
+
+                    <span onClick={() => toggleTab(3)} className="services__button">
+                        View More <i className="uil uil-arrow-right services__button-icon"></i>
+                    </span>
+
+                    <div  className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
+                        <div className="services__modal-content">
+                            <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+
+                            <h3 className="services__modal-title">UI Designer</h3>
+                            <p className="services__modal-description">
+                                Service with various experiences. Providing first to clients and partners.
+                            </p>
+
+                            <ul className="services__modal-services grid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Designing user interface for your app.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Developing a page for your app.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Building your database from scratch.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Creating backend services for your app</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Designing your app portfolio.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+        </section>
+    );
+}
+
+export default Services;
